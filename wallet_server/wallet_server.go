4629ee5baeaa6c6bb3e1a39c18b340ce1de32b40
k-iwashita-prtimes <wonderland.7878@gmail.com>
2022-01-23 00:24:47 +0900

    wallet_serverからblockchain_serverを呼び出す処理

--- wallet_server/wallet_server.go
+++ wallet_server/wallet_server.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"blockchain-study/block"
 	"blockchain-study/utils"
 	"blockchain-study/wallet"
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"html/template"
@@ -90,6 +92,28 @@ func (ws *WalletServer) CreateTransaction(w http.ResponseWriter, req *http.Reque
 		value32 := float32(value)
 
 		w.Header().Add("Content-Type", "application/json")
+
+		transaction := wallet.NewTransaction(privateKey, publicKey,
+			*t.SenderBlockchainAddress, *t.RecipientBlockchainAddress, value32)
+		signature := transaction.GenerateSignature()
+		signatureStr := signature.String()
+
+		bt := &block.TransactionRequest{
+			SenderBlockchainAddress:    t.SenderBlockchainAddress,
+			RecipientBlockchainAddress: t.RecipientBlockchainAddress,
+			SenderPublicKey:            t.SenderPublicKey,
+			Value:                      &value32,
+			Signature:                  &signatureStr,
+		}
+		m, _ := json.Marshal(bt)
+		buf := bytes.NewBuffer(m)
+
+		resp, _ := http.Post(ws.Gateway()+"/transactions", "application/json", buf)
+		if resp.StatusCode == 201 {
+			io.WriteString(w, string(utils.JsonStatus("success")))
+			return
+		}
+		io.WriteString(w, string(utils.JsonStatus("fail")))
 	default:
 		w.WriteHeader(http.StatusBadRequest)
 		log.Println("ERROR: Invalid HTTP Method")
